522967add9398a93d3d0f96f25cbaf85e49d35ed
lyo-n <1y0mmmm@gmail.com>
2021-05-10 14:31:27 +0300

    test_loader

--- src/index.js
+++ src/index.js
@@ -1,7 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+import 'modern-normalize/modern-normalize.css';
+import 'react-toastify/dist/ReactToastify.css';
+import './index.scss';
+import { store, persistor } from './redux/store';
 import App from './components/App';
 
 
-ReactDOM.render(<App />, document.querySelector('#root'));
-
+ReactDOM.render(
+    <React.StrictMode>
+      <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
+          <BrowserRouter>
+            <App />
+          </BrowserRouter>
+        </PersistGate>
+      </Provider>
+    </React.StrictMode>,
+    document.getElementById('root'),
+  );
+  
