9e404d015d0ea9765d858f0a44b30fe972d11d19
div1974 <div@ukr.net>
2021-05-23 12:10:30 +0300

    side bar continue

--- src/components/SideBar/SideBar.jsx
+++ src/components/SideBar/SideBar.jsx
@@ -46,3 +46,43 @@ export default function SideBar() {
     </>
   );
 }
+
+
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+export default function News() {
+  const [articles, setArticles] = useState([]);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+
+    const fetchArticles = () => {
+      return axios
+        .get('https://hn.algolia.com/api/v1/search?query=react')
+        .then(({ data }) => setArticles(data.hits))
+        .catch(error => setError(error))
+        .finally(() => setLoading(false));
+    };
+
+    fetchArticles();
+
+    return () => {
+      // сделать отмену http-запроса чтобы небыло утечки памяти при unmount
+    };
+  }, []);
+
+  return (
+    <ul>
+      {articles.map(({ objectID, title, url }) => (
+        <li key={objectID}>
+          <a href={url} target="_blank" rel="noopener noreferrer">
+            {title}
+          </a>
+        </li>
+      ))}
+    </ul>
+  );
+}
